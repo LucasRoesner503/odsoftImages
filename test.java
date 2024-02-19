e5f4f723a61f6723e3eff0561e75b2a966aebd81
Joao Lucas Roesner <1200613@isep.ipp.pt>
2024-02-19 21:41:00 +0000

    commit test

---
+++ test.java
@@ -0,0 +1,21 @@
+public class test {
+
+    int fmsaofmosad = 0;
+
+    public void test() {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int e = 0;
+        a = 1;
+        b = 2;
+        c = 3;
+        d = 4;
+        e = 5;
+        fmsaofmosad = a + b + c + d + e;
+    }
+}
+
+// Path: test2.java
+
